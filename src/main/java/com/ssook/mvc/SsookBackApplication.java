d76671935897151d180c324ac721a80030bd1333
goodyeon <goodyeon91@gmail.com>
2025-11-21 14:57:24 +0900

    feat : Init 프로젝트 구조

---
+++ src/main/java/com/ssook/mvc/SsookBackApplication.java
@@ -0,0 +1,13 @@
+package com.ssook.mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsookBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsookBackApplication.class, args);
+	}
+
+}
