4f9279fa0390ac61a38531bb71e38f60935377a9
이용당하는이용훈 <ava7890@naver.com>
2025-12-22 14:43:08 +0900

    이미지 추가

--- src/main/java/com/ssook/mvc/enums/UserProfile.java
+++ src/main/java/com/ssook/mvc/enums/UserProfile.java
@@ -11,15 +11,15 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public enum UserProfile {
     // 1단계에서 넣은 파일명에 맞춰 경로를 적어줍니다.
-    IMAGE_1("/images/profile1.png"),
-    IMAGE_2("/images/profile2.png"),
-    IMAGE_3("/images/profile3.png"),
-    IMAGE_4("/images/profile4.png"),
-    IMAGE_5("/images/profile5.png"),
-    IMAGE_6("/images/profile6.png"),
-    IMAGE_7("/images/profile7.png"),
-    IMAGE_8("/images/profile8.png"),
-    DEFAULT("/images/profile9.png"); // 기본 이미지
+    IMAGE_1("/images/profile/profile1.png"),
+    IMAGE_2("/images/profile/profile2.png"),
+    IMAGE_3("/images/profile/profile3.png"),
+    IMAGE_4("/images/profile/profile4.png"),
+    IMAGE_5("/images/profile/profile5.png"),
+    IMAGE_6("/images/profile/profile6.png"),
+    IMAGE_7("/images/profile/profile7.png"),
+    IMAGE_8("/images/profile/profile8.png"),
+    DEFAULT("/images/profile/profile9.png"); // 기본 이미지
 
     private final String url;
 
